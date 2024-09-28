45d30cd3f163616f7a878a122f3d703d5a6288c3
devGabrielPedro <gabrielpedrodsantos@gmail.com>
2024-09-28 16:18:49 -0300

    commit para subir o projeto

---
+++ src/main/java/devtrailsolutis/SqsProducer.java
@@ -0,0 +1,38 @@
+package devtrailsolutis;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+
+import java.net.URI;
+
+public class SqsProducer {
+
+    private static final String QUEUE_URL = "https://sqs.us-east-1.amazonaws.com/000000000000/minha-fila";
+
+    public static void main(String[] args) {
+
+        // Cria o cliente SQS
+        SqsClient sqsClient = SqsClient.builder()
+                .region(Region.US_EAST_1)
+                .endpointOverride(URI.create("http://localhost:4566"))
+                .build();
+
+        // Envia uma mensagem
+        sendMessage(sqsClient, "Envio de mensagem da atividade fila SQS");
+
+        // Fecha o cliente
+        sqsClient.close();
+    }
+
+    public static void sendMessage(SqsClient sqsClient, String message) {
+        SendMessageRequest sendMsgRequest = SendMessageRequest.builder()
+                .queueUrl(QUEUE_URL)
+                .messageBody(message)
+                .delaySeconds(0)
+                .build();
+
+        sqsClient.sendMessage(sendMsgRequest);
+        System.out.println("Mensagem enviada: " + message);
+    }
+}
